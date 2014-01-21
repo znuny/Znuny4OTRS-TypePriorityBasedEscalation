d75fe2c212c1c63ea423846bebf437b985d745d2
Thorsten Eckel <te@znuny.com>
2014-01-21 17:38:31 +0100

    Added SysConfig value translations.

--- Kernel/Language/de_Znuny4OTRSPriorityBasedEscalation.pm
+++ Kernel/Language/de_Znuny4OTRSPriorityBasedEscalation.pm
@@ -12,6 +12,8 @@ sub Data {
     my $Self = shift;
 
     $Self->{Translation}->{"This configuration defines if the first response time should be calculated based on the first owner change or the first agent response."} = "Diese Konfiguration definiert ob die Reaktionszeit beim ersten Besitzerwechsel oder der ersten Kundenantwort gelöscht werden soll.";
+    $Self->{Translation}->{"First owner change"} = 'Erster Besitzerwechsel';
+    $Self->{Translation}->{"First agent response"} = 'Erste Kundenantwort';
 
     return 1;
 }
