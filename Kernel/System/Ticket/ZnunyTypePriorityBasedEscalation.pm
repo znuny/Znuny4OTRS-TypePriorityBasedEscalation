3cbdca2a8f969fe6a4d924ebe151c102de110c6d
Johannes Nickel <jn@znuny.com>
2022-07-26 16:05:54 +0200

    applied code policy

--- Kernel/System/Ticket/ZnunyTypePriorityBasedEscalation.pm
+++ Kernel/System/Ticket/ZnunyTypePriorityBasedEscalation.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2001-2022 OTRS AG, https://otrs.com/
+# Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2012-2022 Znuny GmbH, http://znuny.com/
 # --
 # $origin: znuny - 7b01128479d53da23792fc174c6b51bd183056b7 - Kernel/System/Ticket.pm
